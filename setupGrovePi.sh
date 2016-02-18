f873a0b7b50fc3797b882034f56c16ef79a67fe4
Miodrag Arsic <miodrag.arsic23@gmail.com>
2016-02-18 15:07:10 +0100

    updated shell script

--- setupGrovePi.sh
+++ setupGrovePi.sh
@@ -1,14 +1,13 @@
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
 sudo pip install RPi.GPIO
-cd /home/pi/raspberrypi-python-client	#make certain that git clone is done frmo the right directory.
 sudo pip install paho-mqtt
 git clone https://github.com/DexterInd/GrovePi 	
-sudo cp /home/pi/raspberrypi-python-client/GrovePi/Software/Python/grovepi.py /home/pi/raspberrypi-python-client
-sudo cp /home/pi/raspberrypi-python-client/GrovePi/Software/Python/grovepi.py /home/pi/raspberrypi-python-client/examples
-sudo cp /home/pi/raspberrypi-python-client/GrovePi/Software/Python/grovepi.py /home/pi/raspberrypi-python-client/experiments
-sudo cp /home/pi/raspberrypi-python-client/ATT_IOT.py /home/pi/raspberrypi-python-client/examples
-sudo cp /home/pi/raspberrypi-python-client/ATT_IOT.py /home/pi/raspberrypi-python-client/experiments
+sudo cp GrovePi/Software/Python/grovepi.py 
+sudo cp GrovePi/Software/Python/grovepi.py examples
+sudo cp GrovePi/Software/Python/grovepi.py experiments
+sudo cp ATT_IOT.py examples
+sudo cp ATT_IOT.py experiments
 cd GrovePi/Script
 sudo chmod +x install.sh
 sudo ./install.sh
