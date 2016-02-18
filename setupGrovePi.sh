92cf29df3bf2fe4d3fac89854c221a38768f6540
Miodrag Arsic <miodrag.arsic23@gmail.com>
2016-02-18 15:35:11 +0100

    - tuneup

--- setupGrovePi.sh
+++ setupGrovePi.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install python-pip
 sudo pip install RPi.GPIO
 sudo pip install paho-mqtt
 git clone https://github.com/DexterInd/GrovePi 	
-sudo cp GrovePi/Software/Python/grovepi.py 
+sudo cp GrovePi/Software/Python/grovepi.py .
 sudo cp GrovePi/Software/Python/grovepi.py examples
 sudo cp GrovePi/Software/Python/grovepi.py experiments
 sudo cp ATT_IOT.py examples
